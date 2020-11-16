0a457c8693e9b2e0daa678d038b46e7b26faed44
Jani Ervasti <jani.ervasti@mail.com>
2020-11-16 20:06:12 +0200

    4.21 done

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const cors = require('cors')
-const logger = require('./utils/logger')
-const config = require('./utils/config')
-
-const blogRouter = require('./controllers/blogs')
-const userRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
-
-app.use(bodyParser.json())  
-
-app.use('/api/blogs', blogRouter)
-app.use('/api/users', userRouter)
-app.use('/api/login', loginRouter)
-
-app.use(cors())
-app.use(express.json())
-
-
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
-})
-
-module.exports = app
 
 
